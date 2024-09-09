a064815ea7cec65cc48d9dee745eb1898a6d8794
Nidas <d-n-a_12@live.se>
2024-09-09 11:10:11 +0200

    setup file for first dev release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="PyQT_toolbox",
+    version="0.1dev",
+    packages=[
+        "pyqt_toolbox",
+    ],
+    license="GNU LESSER GENERAL PUBLIC LICENSE, Version 2.1",
+    long_description=open("README.txt").read(),
+)
